4dc9c8ec68c44c0dde0bd2b4c7419895e995d102
tommyb999 <tom_bale@hotmail.com>
2020-04-09 11:38:45 +0100

    Edited the example code from MVC course as mac template was not working for me

---
+++ Hook.Data/Services/InMemoryWorkItemData.cs
@@ -0,0 +1,27 @@
+﻿using Hook.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hook.Data.Services
+{
+    public class InMemoryWorkItemData : IData
+    {
+        List<WorkItem> WorkItems;
+
+        public InMemoryWorkItemData()
+        {
+            WorkItems = new List<WorkItem>()
+            {
+                new WorkItem { Id = 1, Title = "Hook", Developer = "Tom"},
+                new WorkItem { Id = 2, Title = "Something", Developer = "Manish"},
+                new WorkItem { Id = 3, Title = "Something else", Developer = "Josh"},
+            };
+        }
+
+        public IEnumerable<WorkItem> GetAll()
+        {
+            return WorkItems.OrderBy(r => r.Id);
+        }
+    }
+}
---
+++ Hook.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Hook.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hook.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        IData db;
+
+        public HomeController()
+        {
+            db = new InMemoryWorkItemData();
+        }
+
+        public ActionResult Index()
+        {
+            var model = db.GetAll();
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
