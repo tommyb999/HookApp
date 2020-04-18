9487ea80a7c4303bd8ebe2cb7289617fc11cd2a1
tommyb999 <38360050+tommyb999@users.noreply.github.com>
2020-04-18 09:38:49 +0100

    Added OdeToFood.Data

---
+++ OdeToFood.Data/Models/WorkItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hook.Data.Models
+{
+    public class WorkItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public string Product { get; set; }
+        //public DeveloperType Developer { get; set; }
+        public string Developer { get; set; }
+    }
+}
